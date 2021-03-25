36278409ac02bc55bdf8a126271d243566695b64
Oscar Mattsson <oscar.mattsson@accodeing.com>
2021-03-25 13:11:33 +0100

    Commit with conditionals

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable import/no-anonymous-default-export */
+import React, { useState } from 'react';
+import './app.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default () => {
+	const [value, setValue] = useState('');
 
-export default App;
+	const TRAIN = 'train';
+	const BUS = 'bus';
+
+	return (
+		<div className='container'>
+			<button className='container__button' onClick={() => setValue(TRAIN)}>
+				🚆
+			</button>
+			<button className='container__button' onClick={() => setValue(BUS)}>
+				🚌
+			</button>
+
+			{value === TRAIN ? (
+				<div>
+					<h3>Here you can display Train content</h3>
+					<p>And show whatever content you like</p>
+				</div>
+			) : value === BUS ? (
+				<div>
+					<h3>Bus content</h3>
+					<p>And the same here</p>
+				</div>
+			) : (
+				<div>
+					<h3>This is when no conditional is met</h3>
+					<p>No content to show</p>
+				</div>
+			)}
+		</div>
+	);
+};
